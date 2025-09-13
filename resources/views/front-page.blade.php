4f1399d18ebe1f47cf6e0797751af2ba7fe13019
ronanbriot <ronan@kakii.fr>
2025-09-13 12:04:54 +0200

    fix timeline loader

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @include('components.timeline')
-  @endwhile
+  @include('components.timeline')
 @endsection
